9e29423f6571bcb5e685b0e52d2836ee5fe840ae
Armando Valdés <valdes.gam@gmail.com>
2021-08-07 02:37:21 -0500

    subiendo todo

---
+++ js/script_adicional.js
@@ -0,0 +1,10 @@
+// Para que se ponga un menu desplegable en celulares navbar
+$( document ).ready(function(){
+	$(".button-collapse").sideNav();
+})
+
+//para el select de imagenes del formulario
+
+  $(document).ready(function() {
+    $('select').material_select();
+  });
